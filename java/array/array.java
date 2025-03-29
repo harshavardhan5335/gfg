1e4756ac86214f49457b49234337597598d6d729
HarshaLinuxmint <officialytmkbhd@gmail.com>
2025-03-29 22:45:07 +0530

    array

---
+++ java/array/array.java
@@ -0,0 +1,30 @@
+package gfg.java.array;
+
+public class array {
+
+    public static void main(String[] args) {
+        // Declaring
+        int[] array;
+        int array1[];
+
+        // Allocating memory to array
+        array = new int[10];
+
+        // combining both declaring and allocating
+        int[] array2 = new int[10];
+        System.out.println(array2.length);
+
+        // declaring array literal (less java10version)
+        int[] array3 = new int[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+        System.out.println(array.length);
+        for (int num : array3) {
+            System.out.println(num);
+        }
+
+        // declaring array literal (java10version+++)
+        int[] numbers = { 1, 2, 3, 4, 5 };
+        for (int num1 : numbers) {
+            System.out.println(num1);
+        }
+    }
+}
---
+++ java/array/singledim.java
@@ -0,0 +1,11 @@
+package gfg.java.array;
+
+public class singledim {
+    public static void main(String[] args) {
+        int[] singleDimArray = { 1, 2, 3, 4, 5 };
+        for (int num : singleDimArray) {
+            System.out.println(num);
+        }
+    }
+
+}
