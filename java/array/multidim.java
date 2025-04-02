528613c769877dee05bd22df7336bde7885f3f45
harshavardhan5335 <officialytmkbhd@gmail.com>
2025-04-02 11:42:52 +0530

    update

--- java/array/multidim.java
+++ java/array/multidim.java
@@ -2,11 +2,13 @@ package gfg.java.array;
 
 public class multidim {
     public static void main(String[] args) {
-        int[] array = { 1, 2, 3, };
+        int[][] array = { {1, 2, 3}, {1, 2, 3}, {1, 2, 3} };
 
-        for (int num : array) {
-            System.out.println(num);
+        for (int[] row : array) {
+            for (int num : row) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
         }
     }
-
 }
