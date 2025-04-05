6c4b04d1420e1162bd2add237128681184e7d31e
harshavardhan5335 <officialytmkbhd@gmail.com>
2025-04-05 12:44:21 +0530

    udpate

--- java/patterns/pattern8.java
+++ java/patterns/pattern8.java
@@ -2,18 +2,18 @@ package gfg.java.patterns;
 
 import java.util.Scanner;
 
-public class pattern6 {
+public class pattern8 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a rows : ");
-        int rows = sc.nextInt();
-        for (int i = 1; i <= rows; i++) {
-            for (int j = rows; j >= i; j--) {
+        System.out.print("Enter a number : ");
+        int row = sc.nextInt();
+        for (int i = row; i >= 1 ; i--) {
+            for (int j = i; j >=1 ; j--) {
                 System.out.print(j + " ");
             }
             System.out.println();
         }
         sc.close();
     }
-
+    
 }
