6e6bac4dc8da8dbc94e79e70c93505da418ba804
harshavardhan5335 <officialytmkbhd@gmail.com>
2025-04-06 21:40:38 +0530

    {commit_message}

--- java/patterns/pattern1.java
+++ java/patterns/pattern1.java
@@ -10,7 +10,7 @@ public class pattern1 {
         int n = sc.nextInt();
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
-                System.out.print(j + " ");
+                System.out.print(j + " ");  
             }
             System.out.println(" ");
         }
